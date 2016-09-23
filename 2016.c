828f42e9e18e74afbddbc51d0ec874033c117f95
LEONARDO VINICIUS CARVALHO ZANELLA <lvcz10@inf.ufpr.br>
2016-09-23 20:28:22 -0300

    convergindo

--- 2016.c
+++ 2016.c
@@ -5,16 +5,17 @@
 #define M_PI 3.1415926535
 
 
-
-double *generateSquareRandomPositiveDefiniteMatrix( unsigned int n );
+void metodo_gradiente(double *a, double *b, int n, double tol, int maxIter,int nBandas);
 int generateRandomDiagonal( unsigned int N, unsigned int k, unsigned int nBandas, double *diag );
 void imprime_sistema(double *A, double *b, int n) ;
 double *calcula_func_b(int n);
 double vetorT_x_vetor(double *a, double *b ,int n);
 void copia_vetor(double *a, double *b,int n);
 double *aloca_vetor(int n);
-void matriz_x_vetor(double *a, double *v,double *z, int n);
+void matriz_x_vetor(double *a, double *x,double *b, int n, int nBandas);
 double vetor_x_num(double num, double *a,int n);
+void vetor_mais_vetor(double *a, double *b, double *res, int n);
+void calcula_residuo(double *a,double *x,double *b,int n, int nBandas);
 
 /***********************
  * N: tamanho do sistema linear
@@ -59,23 +60,25 @@ void main (int argc, char** argv){
     FILE *arquivo_saida;
 	srand(20162);	
 	
-	if(argc >1){
-		if (!strcmp("n", argv[1]))
-        {
+	if(argc >2){
+		
             int aux;
-            aux = atoi(argv[2]);
+            aux = atoi(argv[1]);
             if (aux > 0 ) {
                 n = aux;
             } else {
                 fprintf (stderr, "\nN deve ser n > 0 ");
             return;
-          }
-        }
-		else {
-            fprintf (stderr, "\nN deve ser maior que 0 ");
-            return;
-        }
-		
+			}
+			aux = atoi(argv[2]);
+				if (aux >= 0) {
+					nBandas = aux;
+				} else {
+					fprintf (stderr, "\n Erro ao definir o numero maximo de iterações.");
+				return ;
+			  }
+        
+				
 		for (int i = 3; i < argc; i += 2) {
 		
 		   
@@ -141,10 +144,11 @@ void main (int argc, char** argv){
 	
 
 	b = calcula_func_b(n);
+	for (int i = 0; i < n; ++i) {
+		printf( "%d:%lf \n ", i,b[i]);
+		}
 
-	//imprime_sistema(A,b,n);
-
-	//GaussSeidel(A,b,x,n);
+	metodo_gradiente(A,b,n,0,10,nBandas);	
 	
 
 	
@@ -172,50 +176,6 @@ double *calcula_func_b(int n)
 	return (bx);
 }
 
-
-
-
-
-double *generateSquareRandomPositiveDefiniteMatrix( unsigned int n )
-{
-  double *mat = NULL;
-
-  /* return NULL if memory allocation fails */
-  if ( ! (mat = (double *) malloc(n*n*sizeof(double))) )
-    return (NULL);
-
-  /* generate a randomly initialized matrix in row-major order */
-  double *ptr = mat;
-  double *end = mat + n*n;
-
-  double invRandMax = 1.0/(double)RAND_MAX;
-
-  while( ptr != end ) {
-    *ptr++ = (double)rand() * invRandMax;
-  }
-
-  /* Now we want to make this matrix positive definite. Since all
-     values are positive and <1.0, we have to ensure it is symmetric
-     and diagonally dominant.
-
-     A = A + transpose(A)
-     A = A + I*n                        */
-  unsigned int i,j;
-  for (i=0; i<n; ++i)
-    for (j=i+1; j<n; ++j)
-    {
-      double aux = mat[i*n+j];
-      mat[i*n+j] += mat[j*n+i];
-      mat[j*n+i] += aux;
-    }
-
-  for (i=0; i<n; ++i)
-    mat[i*n+i] += mat[i*n+i] + n;
-
-  return (mat);
-}
-
-
 void imprime_sistema(double *A, double *b, int n) 
 {
   
@@ -234,14 +194,24 @@ double vetorT_x_vetor(double *a, double *b, int n)
 {
     double aux =0.0d;
     for (int i = 0 ; i<n ;++i) 
-        aux +=a[i]*b[i];
-    
+        aux +=a[i]*b[i];    
     return aux;
 }
 double vetor_x_num(double num, double *a,int n)
 {
 	for (int i = 0 ; i<n ;++i)
         a[i] = a[i]*num;
+}
+void vetor_mais_vetor(double *a, double *b, double *res, int n){
+	
+for (int i = 0 ; i<n ;++i) 
+        res[i] = a[i] + b[i];
+}
+
+void vetor_menos_vetor(double *a, double *b, double *res, int n){
+	
+for (int i = 0 ; i<n ;++i) 
+        res[i] = a[i] - b[i];
 }
 
 void copia_vetor(double *a, double *b,int n)
@@ -260,7 +230,7 @@ double *aloca_vetor(int n)
 
 void matriz_x_vetor(double *a, double *x,double *b, int n, int nBandas)
 {
-	//z[0]= a[0]*v[0] + a[n]*v[1]
+	
     for (int i =0; i< n ;++i){
 		b[i]= a[i] * x[i];
 		
@@ -277,7 +247,7 @@ void matriz_x_vetor(double *a, double *x,double *b, int n, int nBandas)
 /*
 		Calcula metodo do gradiente seguindo o livro.
 */
-void metodo_gradiente(double *a, double *b, double *x, int n, double tol, int maxIter,int nBandas)
+void metodo_gradiente(double *a, double *b, int n, double tol, int maxIter,int nBandas)
 {
 	double *x_result = aloca_vetor(n);
 	
@@ -292,6 +262,7 @@ void metodo_gradiente(double *a, double *b, double *x, int n, double tol, int ma
     copia_vetor(v,b,n);
 	
 	double *z = aloca_vetor(n);	
+	double *mr = aloca_vetor(n);	
 	
 	double s = 0.0d;
 	
@@ -301,44 +272,84 @@ void metodo_gradiente(double *a, double *b, double *x, int n, double tol, int ma
 	double v_x_z=0.0d;
 	int k=0;
 	
-	
+	printf( "aux =%lf \n ", aux);
 	
 	while( k < maxIter )
 	{	
+		
 		//calcula z	 = A * v
-		matriz_x_vetor(a,v,z,n);
+		matriz_x_vetor(a,v,z,n,nBandas);
 		
 		//calcula  s= aux / v * z	
-		
+		//
 		v_x_z = vetorT_x_vetor(v,z,n);	
+		printf( "vx =%lf \n ", v_x_z);
 		
-		s=aux/v_x_z;
+		s = aux/v_x_z;
+		
+		printf( "S =%lf \n ", s);
+		//
+		
 		
 		//calcula x(k+1) = x(k) + s*v		
+		//
 		copia_vetor(x_ant,x_result,n);
-		vetor_x_num(s,x_result,n);
+		
+		vetor_x_num(s,v,n);
+		
+		vetor_mais_vetor(x_result,v,x_result,n);
 		//
 		
 		norma = 0.0d;
 		//calcula  r = r- sz
-		for (int i = 0 ; i < n;++i)
-		{
-			//r[i] -= s[i] * z[i];
-			//aux1[i] = r[i] * r[i];
-			//norma += aux1[i];
-		}
+		vetor_x_num(s,z,n);
+		vetor_menos_vetor(r,z,r,n);
+		
+		//aux1= r*r
+		aux1 = vetorT_x_vetor(r,r,n);
+		printf( "aux1 =%lf \n ", aux1);
 		// caso convergiu retorna x-result
 		//if (norma <tol){return;} 
 		
 		//caso não converja
-		// m= aux1/aux; aux = aux1; v = r+m*r
+		// m= aux1/aux; aux = aux1;
+		m = aux1 / aux;
+		aux = aux1;
 		
-		for( int i = 0;i<n ;++i){
-			//m[i] = aux1[i] / aux[i];
-			//aux[i] = aux1[i];
-			
-			//v[i] = r[i] + m[i] * r[i];
-			
+		// v = r+m*r
+		 copia_vetor(mr,r,n);
+		 vetor_x_num(m,mr,n);
+		 vetor_mais_vetor(r,mr,v,n);
+		
+		
+		
+		
+		k++;
+		
+		
+		
+		
+		
+		
+		for (int i = 0; i < n; ++i) {
+		printf( "%d:%lf \n ", i,x_result[i]);
 		}
+		calcula_residuo(a,x_result,b,n,nBandas);
+		printf( "\n ");
+		
 	}
+}
+
+void calcula_residuo(double *a,double *x,double *b,int n, int nBandas)
+{
+	double *res= aloca_vetor(n);
+	
+	matriz_x_vetor(a,x,res,n,nBandas);
+	vetor_menos_vetor(b,res,res,n);
+	
+	for (int i = 0; i < n; ++i) {
+		printf( "%d:%lf \n ", i,res[i]);
+		}
+		
+		
 }
